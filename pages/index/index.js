5471a7b9e0b63d5d2d97988530ef086c4255d117
刘河垒 <lhl@dxy.cn>
2018-11-25 23:28:20 +0800

    页面调整

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,12 +1,12 @@
 //index.js
 //获取应用实例
 const app = getApp()
-var util = require('../../utils/util.js');
 
 Page({
   data: {
     motto: 'Hello World',
     userInfo: {},
+    question: {},
     nowDate: null,
     canIUse: wx.canIUse('button.open-type.getUserInfo')
   },
@@ -14,13 +14,36 @@ Page({
   onLoad: function () {
     var myDate = new Date();
     var hour = myDate.getHours();
-    console.log(hour);
-    
-    var time = util.formatTime(new Date());
-    // 再通过setData更改Page()里面的data，动态更新页面的数据
-    this.setData({
-      nowDate: time
-    });
+   
+    if (hour >= 3 && hour<= 5){
+      this.setData({
+        nowDate: '凌晨了，怎么还不睡？'
+      })
+    }else if ((hour >= 22 && hour<= 23) || (hour >= 0 && hour<= 2)) {
+      this.setData({
+        nowDate: '夜已经深了，该睡觉了？'
+      })
+    }else if (hour >= 5 && hour<= 8) {
+      this.setData({
+        nowDate: '早上好，开始新的一天吧'
+      })
+    }else if (hour >= 8 && hour<= 11) {
+      this.setData({
+        nowDate: '上午好'
+      })
+    }else if (hour >= 12 && hour<= 13) {
+      this.setData({
+        nowDate: '中午好'
+      })
+    }else if (hour >=14  && hour<= 18) {
+      this.setData({
+        nowDate: '下午好'
+      })
+    }else if (hour >= 19 && hour<= 22) {
+      this.setData({
+        nowDate: '晚上好'
+      })
+    }
 
     if (app.globalData.userInfo) {
       this.setData({
@@ -48,12 +71,68 @@ Page({
         }
       })
     }
+    this.getQuestion();
   },
+/**
+ * 用户信息
+ * @param {*} e 
+ */
   getUserInfo: function(e) {
     app.globalData.userInfo = e.detail.userInfo
     this.setData({
       userInfo: e.detail.userInfo,
       hasUserInfo: true
     })
+  },
+/**
+ * 翻页和保存
+ * @param {*} e 
+ */
+  changeQuestion: function(e) {
+    var self = this;
+    wx.showLoading({});
+    
+    wx.request ({
+      url: "http://dxy.local/test/previous.php",
+      method: "POST",
+      data:{
+        'qid': e.currentTarget.id,
+        'type': e.target.id,
+      },
+      header: {
+        // 'content-type': 'application/json' // 默认值
+        'content-type': 'application/x-www-form-urlencoded' // 默认值
+      },
+      success: function(reg) {
+        wx.hideLoading();
+        self.setData({
+          question : reg.data
+        })
+      }
+    })
+  },
+
+  /**
+   * 首页内容加载
+   */
+  getQuestion: function() {
+    var self = this;
+    wx.request ({
+      url: "http://dxy.local/test/previous.php",
+      method: "GET",
+      
+      success: function(reg) {
+        self.setData({
+          question : reg.data
+        })
+      }
+    })
+  },
+  onPageScroll:function(e){
+    if(e.scrollTop<0){
+      wx.pageScrollTo({
+        scrollTop: 0
+      })
+    }
   }
 })
